6c71933702df0315d1265559f2851956640a5554
Mychi <mickdd22@gmail.com>
2021-10-14 22:52:17 +0000

    :sparkles: added _env for leaf 2 support

--- src/functions.php
+++ src/functions.php
@@ -1,5 +1,25 @@
 <?php
 
+if (!function_exists('_env')) {
+    /**
+     * Gets the value of an environment variable.
+     *
+     * @param  string  $key
+     * @param  mixed  $default
+     * @return mixed
+     */
+    function _env($key, $default = null)
+    {
+        $item = getenv($key);
+
+        if (!isset($_ENV[$key]) || (isset($_ENV[$key]) && $_ENV[$key] == null)) {
+            $item = $default;
+        }
+
+        return $item;
+    }
+}
+
 if (!function_exists("render")) {
     function render(string $view, array $data = [])
     {
