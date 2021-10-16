2978007719b0413b863cb159bbcfe8343b18dbce
Mychi <mickdd22@gmail.com>
2021-10-16 11:58:19 +0000

    :sparkles: added new view method

--- src/functions.php
+++ src/functions.php
@@ -20,9 +20,35 @@ if (!function_exists('_env')) {
     }
 }
 
-if (!function_exists("render")) {
-    function render(string $view, array $data = [])
+if (!function_exists("view")) {
+    function view(string $view, array $data = [])
     {
-        return ViewConfig("render")($view, $data);
+        if (ViewConfig("render")) {
+            if (ViewConfig("config")) {
+                call_user_func_array(ViewConfig("config"), [[
+                    "views_path" => AppConfig("views.path"),
+                    "cache_path" => AppConfig("views.cachePath"),
+                ]]);
+            }
+
+            return ViewConfig("render")($view, $data);
+        }
+
+        $engine = ViewConfig("view_engine");
+
+        $className = strtolower(get_class(new $engine));
+
+        $fullName = explode("\\", $className);
+        $className = $fullName[count($fullName) - 1];
+
+        if (forward_static_call_array(["Leaf\\View", $className], [])) {
+            forward_static_call_array(["Leaf\\View", $className], [])->configure(AppConfig("views.path"), AppConfig("views.cachePath"));
+            return forward_static_call_array(["Leaf\\View", $className], [])->render($view, $data);
+        }
+
+        $engine = new $engine($engine);
+        $engine->config(AppConfig("views.path"), AppConfig("views.cachePath"));
+
+        return $engine->render($view, $data);
     }
 }
