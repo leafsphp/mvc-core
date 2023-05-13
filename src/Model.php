5bb36d4f6365979ceb348b4ca5af0011cde662bf
1mahdimf <1mahdimf@users.noreply.github.com>
2023-05-13 12:32:24 +0330

    Update Model.php
    Model::create([]); work

--- src/Model.php
+++ src/Model.php
@@ -6,8 +6,8 @@ Database::connect();
 
 class Model extends \Illuminate\Database\Eloquent\Model
 {
-    public function __construct()
+    public function __construct(array $attributes = [])
     {
-        parent::__construct();
+        parent::__construct($attributes);
     }
 }
