9e6b8e2d9ca12026ba57787d8380ac46d06664d5
mychidarko <mickdd22@gmail.com>
2023-07-03 22:37:37 +0000

    feat: switch from file paths to loaded paths

--- src/Core.php
+++ src/Core.php
@@ -11,6 +11,9 @@ class Core
 {
     protected static $paths;
 
+    protected static $config = [];
+
+
     public static function paths($paths = null)
     {
         if (!$paths) {
@@ -18,5 +21,57 @@ class Core
         }
 
         static::$paths = $paths;
+    }
+
+    /**
+     * Setup MVC application based on config
+     */
+    public static function loadApplicationConfig()
+    {
+        static::paths(PathsConfig());
+        static::loadConfig();
+
+        Database::config(static::config('database'));
+
+        if (php_sapi_name() !== 'cli') {
+            app()->config(static::config('app'));
+            app()->cors(static::config('cors'));
+        }
+    }
+
+    /**
+     * Load all config files defined in the config folder
+     */
+    protected static function loadConfig()
+    {
+        $configPath = static::$paths["config"];
+        $configFiles = glob("$configPath/*.php");
+
+        foreach ($configFiles as $configFile) {
+            $configName = basename($configFile, ".php");
+            $config = require $configFile;
+            static::config($configName, $config);
+        }
+    }
+
+    /**
+     * Set/Get config
+     */
+    public static function config($config = null, $value = null)
+    {
+        if (!$config) {
+            return static::$config;
+        }
+
+        if (is_array($config)) {
+            static::$config = array_merge(static::$config, $config);
+            return;
+        }
+
+        if (!$value) {
+            return static::$config[$config];
+        }
+
+        static::$config[$config] = $value;
     }
 }
