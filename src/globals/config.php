8efaec512859abb7901f8566e92be1f37bd41aa2
mychidarko <mickdd22@gmail.com>
2025-01-05 22:46:14 +0000

    fix: patch up mvc config loading

--- src/globals/config.php
+++ src/globals/config.php
@@ -19,6 +19,7 @@ function PathsConfig($setting = null)
         'channels' => 'app/channels',
         'components' => 'app/components',
         'controllers' => 'app/controllers',
+        'database' => 'app/database',
         'databaseStorage' => 'storage/app/db',
         'exceptions' => 'app/exceptions',
         'events' => 'app/events',
@@ -94,5 +95,5 @@ function MailConfig($setting = null)
 function MvcConfig($appConfig, $setting = null)
 {
     $config = \Leaf\Config::getStatic("mvc.config.$appConfig");
-    return !$setting ? $config : $config[$setting];
+    return !$setting ? $config : ($config[$setting] ?? null);
 }
